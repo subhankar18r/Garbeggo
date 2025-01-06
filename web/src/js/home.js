9bc4f394f460d3e0271bbf63d6fc094bda92c8c8
Subhankar Rajbanshi <subhankar18r@gmail.com>
2025-01-06 15:25:48 +0530

    add map &  change onvalue to get one time from rtdb

--- web/src/js/home.js
+++ web/src/js/home.js
@@ -1,18 +1,52 @@
-import { onValue, ref } from "firebase/database";
+import { child, get, ref } from "firebase/database";
 import { RTDB } from "./firebase.js";
+import Map from "../components/map.js";
 
 const OpenEmptyDustBtn = document.querySelector("#open-empty-dustbin-btn");
-
-const unfilledDustContainer = document.querySelector(".unfilled");
 const arrowImage = OpenEmptyDustBtn.querySelector("img");
+const filledDustContainer = document.querySelector(".filled");
+const unfilledDustContainer = document.querySelector(".unfilled");
+const mapContainer = document.querySelector(".map");
+
+mapContainer.innerHTML = Map();
 
 OpenEmptyDustBtn.addEventListener("click", () => {
   unfilledDustContainer.classList.toggle("hide");
   arrowImage.classList.toggle("rotate-90");
 });
 
-const starCountRef = ref(RTDB, "Dustbins");
-onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
-  console.log(data);
-});
+const dbRef = ref(RTDB);
+get(child(dbRef, "dustbins"))
+  .then((snapshot) => {
+    if (!snapshot.exists()) {
+      filledDustContainer.innerHTML = "No data available";
+      console.log("No data available");
+    }
+    const data = snapshot.val();
+    let hasEmpty = false;
+    for (const dustbinId in data) {
+      if (data.hasOwnProperty(dustbinId)) {
+        const dustbin = data[dustbinId];
+        if (dustbin.status == "full") {
+          filledDustContainer.innerHTML += `
+        <li>${dustbinId}</li>
+        `;
+          const item = document.querySelector(`#${dustbinId}`);
+          if (item) {
+            item.style.fill = "red";
+          }
+        } else if (dustbin.status == "empty") {
+          hasEmpty = true;
+          unfilledDustContainer.innerHTML += `
+        <li>${dustbinId}</li>
+        `;
+        }
+      }
+    }
+    if (!hasEmpty) {
+      unfilledDustContainer.innerHTML = "No empty dustbins available";
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
